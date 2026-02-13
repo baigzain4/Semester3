948994739c0524f25706c32aa8c8295805bbeac4
MirzaZain <baigzain498@gmail.com>
2026-02-14 02:01:32 +0500

    DSA new file updated

---
+++ DSA Lab/week1/1_LinearSearch.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int linearSearch(int arr[], int size, int target){
+    for(int i = 0;i<size;i++){
+        if(arr[i]== target){
+            return i;
+        }
+    }
+    return 1;
+}
+int main(){
+    int size, search;
+    int *array = new int[size];
+    cout << "Enter the size of an array: ";
+    cin >> size;
+    for(int i = 0;i<size;i++){
+        cout << "Enter the element for index " << i << ": ";
+        cin >> array[i];
+    }
+    cout << "Enter the search: ";
+    cin >> search;
+
+    int result = linearSearch(array,size,search);
+    if(search != 1){
+        cout << "Your Search found at index: " << result << endl;
+    }
+    else{
+        cout << "Your search not found. " << endl;
+    }
+
+    return 0;
+}
