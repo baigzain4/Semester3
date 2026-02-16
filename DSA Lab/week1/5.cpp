0beeeb05dad72732369b0e2870a5f40e6b3cdca0
My-journey <baigzain498@gmail.com>
2026-02-16 23:22:51 +0500

    CN Lab Material added.

--- DSA Lab/week1/5.cpp
+++ DSA Lab/week1/5.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[3][2]= {{1,2},{3,4},{5,6}};
+    int arr[3][2]= {{1,2},{0,4},{5,3}};
     int target = 3;
     int row,column;
     for(int i = 0;i<3;i++){
-        for(int j =0;j<=i;j++){
+        for(int j =0;j<2;j++){
             if(arr[i][j] == target){
                 row = i;
                 column = j;
@@ -15,4 +15,7 @@ int main(){
         cout << "OK" << endl;
     }
     cout << row << " " << column << endl;
+    return 0;
 }
+
+// O(1)
